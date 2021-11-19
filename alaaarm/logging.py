da4efc5fce8e6f310e57066dd27d40591dad3a6c
Stefan <stefan@neuland.io>
2021-11-19 20:08:37 +0100

    logging refactoring

--- alaaarm/logging.py
+++ alaaarm/logging.py
@@ -2,24 +2,31 @@ import sys
 import usocket as socket
 
 
+class Severity:
+  CRITICAL = 2
+  ERROR = 3
+  WARN = 4
+  INFO = 6
+  DEBUG = 7
+
+
 class SyslogMessage:
-    def __init__(self, message, severity=7):
+    @classmethod
+    def from_log_record(cls, record):
+        return cls(record.message,
+                   severity=getattr(Severity, record.levelname))
+
+    def __init__(self, message, severity=7, facility=1):
         self.message = message
         self.severity = severity
-
-    @property
-    def facility(self):
-        return 1    # USER
+        self.facility = facility
 
     @property
     def pri(self):
         return self.severity + (self.facility << 3)
 
     def __str__(self):
-        return '<{pri}>{message}'.format(
-            pri=self.pri,
-            message=self.message
-        )
+        return f'<{self.pri}>{self.message}'
 
 
 class SyslogClient:
@@ -33,19 +40,18 @@ class SyslogClient:
     @property
     def dst(self):
         if not self._dst:
-            self._dst = self._resolve_dst()
+            self._resolve_dst()
 
         return self._dst
 
     def send(self, msg):
-        if self.dst:
-            self.socket.sendto(str(msg).encode(), self.dst[0][4])
+        try:
+            self.socket.sendto(str(msg).encode(), self.dst[0][-1])
+        except OSError:
+            pass
 
     def _resolve_dst(self):
-        try:
-            return socket.getaddrinfo(self._host, self._port)
-        except OSError:
-            return None
+        self._dst = socket.getaddrinfo(self._host, self._port)
 
 
 class RemoteHandler:
@@ -53,24 +59,7 @@ class RemoteHandler:
         self.syslog_client = SyslogClient(host, port)
 
     def emit(self, record):
-        self.syslog_client.send(self._to_syslog_message(record))
-
-    def _to_syslog_message(self, record):
-        return SyslogMessage(
-            record.message,
-            severity=self._levelname_to_syslog_severity(record.levelname)
-        )
-
-    def _levelname_to_syslog_severity(self, levelname):
-        levelname_to_severity = {
-            'CRITICAL': 2,
-            'ERROR': 3,
-            'WARN': 4,
-            'INFO': 6,
-            'DEBUG': 7,
-        }
-
-        return levelname_to_severity[levelname]
+        self.syslog_client.send(SyslogMessage.from_log_record(record))
 
 
 class ConsoleHandler:
